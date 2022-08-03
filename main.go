41a0c7fc8064ba6d68a20564b45a3af3bfcb004c
Kelvin Tri <kelvintriyansyah@gmail.com>
2022-08-03 19:36:39 +0800

    cors

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"os"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 )
 
 func main() {
@@ -17,6 +18,7 @@ func main() {
 	//routes
 	routes.UserRoute(app) //add this
 	routes.TaskRoute(app)
+	app.Use(cors.New())
 
 	port := os.Getenv("PORT")
 	if port == "" {
