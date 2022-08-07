7ae9b07ce2934200b81d4ad03efe3d11f3603102
Kelvin Tri <kelvintriyansyah@gmail.com>
2022-08-07 18:33:30 +0800

    upd typo

--- models/task_model.go
+++ models/task_model.go
@@ -8,8 +8,8 @@ import (
 
 type Task struct {
 	Id          primitive.ObjectID `json:"id,omitempty"`
-	TaskName    string             `json:"taskname,omitempty `
-	Assignee    string             `json:"assignee,omitempty `
+	TaskName    string             `json:"taskname,omitempty"`
+	Assignee    string             `json:"assignee,omitempty"`
 	TaskDone    bool               `json:"taskdone"`
 	CreatedDate time.Time          `json:"createddate"`
 	DeadLine    string             `json:"deadline"`
